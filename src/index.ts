77d0dbdac9e9937483fab9954cdafb8c9a290394
Flávio Félix <fau_felix30@hotmail.com>
2023-09-08 21:03:04 -0300

    Pequeno projeto typescript

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as Functions from './myFunctions';
+import users from './data';
+
+console.log(`Lista todas as pessoas usuárias do Github: ${Functions.getUserNames(users)}`);
+
+console.log(`Pessoas com pelo menos 20 repositórios: ${Functions.getUsersByRepoQuantity(users, 20)}`);
+
+console.log(`Essa é a conta mais ativa da lista: ${Functions.IsMostActiveUser('João Paulo Aramuni', users)}`);
